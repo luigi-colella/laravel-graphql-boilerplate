f94c9b2b846b0f93bf3ba8f80d14af50079dd344
lgcolella <luigicolella.1997@gmail.com>
2020-02-08 16:28:32 +0100

    api: added graphql endpoint

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,9 +5,18 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * The GraphQL endpoint
+     */
+    public function graphql(): JsonResponse
+    {
+        return response()->json();
+    }
 }
