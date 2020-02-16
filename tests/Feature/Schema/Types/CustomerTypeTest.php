77b9e8025b1c1ab1e9f73666ff70ab77b5a3fe62
lgcolella <luigicolella.1997@gmail.com>
2020-02-16 20:10:20 +0100

    test: added test for list of customer type

--- tests/Feature/Schema/Types/CustomerTypeTest.php
+++ tests/Feature/Schema/Types/CustomerTypeTest.php
@@ -10,7 +10,7 @@ class CustomerTypeTest extends TestCase
     /**
      * @test
      */
-    public function graphql_endpoint_returns_customer_type_correctly()
+    public function graphql_endpoint_returns_customer_type()
     {
         $model = factory(Customer::class)->create();
 
@@ -37,5 +37,41 @@ class CustomerTypeTest extends TestCase
         ])
             ->assertSuccessful()
             ->assertJsonPath('data.customer', $model->toArray());
+    }
+
+    /**
+     * @test
+     */
+    public function graphql_endpoint_returns_list_of_customer_type()
+    {
+        $model = factory(Customer::class)->create();
+
+        $json = $this->post(self::GRAPHQL_ENDPOINT, [
+            'query' => "
+                {
+                    customers {
+                        edges {
+                            node {
+                                customerNumber
+                                customerName
+                                contactLastName
+                                contactFirstName
+                                phone
+                                addressLine1
+                                addressLine2
+                                city
+                                state
+                                postalCode
+                                country
+                                salesRepEmployeeNumber
+                                creditLimit
+                            }
+                        }
+                    }
+                }
+            ",
+        ])
+            ->assertSuccessful()
+            ->assertJsonPath('data.customers.edges.0.node', $model->toArray());
     }
 }
