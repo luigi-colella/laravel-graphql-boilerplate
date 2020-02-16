f6deb7eba315ecb474d9781526bad2a37d6c7dd4
lgcolella <luigicolella.1997@gmail.com>
2020-02-16 01:24:32 +0100

    test: added feature tests for graphql endpoint

---
+++ database/migrations/2020_02_15_015615_create_customer_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->bigIncrements('customerNumber');
+            $table->string('customerName', 50)->nullable(false);
+            $table->string('contactLastName', 50)->nullable(false);
+            $table->string('contactFirstName', 50)->nullable(false);
+            $table->string('phone', 50)->nullable(false);
+            $table->string('addressLine1', 50)->nullable(false);
+            $table->string('addressLine2', 50)->nullable();
+            $table->string('city', 50)->nullable(false);
+            $table->string('state', 50)->nullable();
+            $table->string('postalCode', 15)->nullable();
+            $table->string('country', 50)->nullable(false);
+            $table->string('salesRepEmployeeNumber', 11)->nullable();
+            $table->decimal('creditLimit', 10,2)->nullable();
+            
+            //$table->primary('customerNumber');
+            //$table->foreign('salesRepEmployeeNumber')->references('employeeNumber')->on('employees');
+
+            $table->engine = 'InnoDB';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer');
+    }
+}
---
+++ tests/Feature/ControllerTest.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Customer;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    use RefreshDatabase;
+
+    CONST GRAPHQL_ENDPOINT = '/api/graphql';
+
+    /**
+     * @test
+     */
+    public function graphql_endpoint_returns_an_error_if_no_id_is_specified_for_a_single_model()
+    {
+        $this->post(self::GRAPHQL_ENDPOINT, [
+            'query' => '
+                {
+                    customer {
+                        customerName
+                    }
+                }
+            ',
+        ])
+            ->assertSuccessful()
+            ->assertJsonPath('errors.0.message', 'Field "customer" argument "id" of type "Int!" is required but not provided.');
+    }
+
+    /**
+     * @test
+     */
+    public function graphql_endpoint_returns_a_single_model()
+    {
+        $model = factory(Customer::class)->create();
+
+        $this->post(self::GRAPHQL_ENDPOINT, [
+            'query' => "
+                {
+                    customer (id: ". $model->customerNumber .") {
+                        customerName
+                    }
+                }
+            ",
+        ])
+            ->assertSuccessful()
+            ->assertJsonPath('data.customer.customerName', $model->customerName);
+    }
+
+    /**
+     * @test
+     */
+    public function graphql_endpoint_returns_a_list_of_models_with_pagination_info()
+    {
+        factory(Customer::class)->create();
+        $model2 = factory(Customer::class)->create();
+        $model3 = factory(Customer::class)->create();
+        $model4 = factory(Customer::class)->create();
+        $model5 = factory(Customer::class)->create();
+
+        $this->post(self::GRAPHQL_ENDPOINT, [
+            'query' => "
+                {
+                    customers (after: ". $model2->customerNumber .", first: 2) {
+                        totalCount
+                        edges {
+                            cursor
+                            node {
+                                customerName
+                            }
+                        }
+                        pageInfo {
+                            endCursor
+                            hasNextPage
+                        }
+                    }
+                }
+            ",
+        ])
+            ->assertSuccessful()
+            ->assertJsonPath('data.customers', [
+                'totalCount' => 5,
+                'edges' => [
+                    [
+                        'cursor' => $model3->customerNumber,
+                        'node' => [
+                            'customerName' => $model3->customerName,
+                        ],
+                    ],
+                    [
+                        'cursor' => $model4->customerNumber,
+                        'node' => [
+                            'customerName' => $model4->customerName,
+                        ]
+                    ],
+                ],
+                'pageInfo' => [
+                    'endCursor' => $model5->customerNumber,
+                    'hasNextPage' => true,
+                ],
+            ]);
+    }
+
+    /**
+     * @test
+     */
+    public function graphql_endpoint_returns_a_list_of_models_with_has_next_page_field_equal_to_false()
+    {
+        $model = factory(Customer::class)->create();
+
+        $this->post(self::GRAPHQL_ENDPOINT, [
+            'query' => "
+                {
+                    customers {
+                        edges {
+                            cursor
+                        }
+                        pageInfo {
+                            hasNextPage
+                        }
+                    }
+                }
+            ",
+        ])
+            ->assertSuccessful()
+            ->assertJsonPath('data.customers.pageInfo.hasNextPage', false);
+    }
+}
