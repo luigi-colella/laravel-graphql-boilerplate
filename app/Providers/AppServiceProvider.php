908dea4c3c149d90c5d4322dc37f76244bca2013
lgcolella <luigicolella.1997@gmail.com>
2020-02-23 04:26:36 +0100

    feature: added log for queries

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +26,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Log queries if you are in the local environment
+        if (app()->isLocal()) {
+            DB::listen(function (QueryExecuted $query) {
+                $time = $query->time;
+                $query = vsprintf(str_replace('?', '%s', $query->sql), $query->bindings);
+                Log::info("A query was executed in {$time}s: \"{$query}\"");
+            });
+        }
     }
 }
