39013f5d3c52a20d3714541269e5487de2e610e3
lgcolella <luigicolella.1997@gmail.com>
2020-02-29 20:44:52 +0100

    refactor: lazy loading for root fields

--- app/Schema/Types/QueryType.php
+++ app/Schema/Types/QueryType.php
@@ -19,24 +19,26 @@ class QueryType extends ObjectType
     public function __construct()
     {
         parent::__construct([
-            'fields' => [
-                'customer' => FieldRegistry::rootModel(TypeRegistry::customer(), Customer::class),
-                'customers' => FieldRegistry::rootList(TypeRegistry::customer(), Customer::class),
-                'employee' => FieldRegistry::rootModel(TypeRegistry::employee(), Employee::class),
-                'employees' => FieldRegistry::rootList(TypeRegistry::employee(), Employee::class),
-                'office' => FieldRegistry::rootModel(TypeRegistry::office(), Office::class),
-                'offices' => FieldRegistry::rootList(TypeRegistry::office(), Office::class),
-                'order' => FieldRegistry::rootModel(TypeRegistry::order(), Order::class),
-                'orders' => FieldRegistry::rootList(TypeRegistry::order(), Order::class),
-                'orderDetail' => FieldRegistry::rootModel(TypeRegistry::orderDetail(), OrderDetail::class),
-                'orderDetails' => FieldRegistry::rootList(TypeRegistry::orderDetail(), OrderDetail::class),
-                'payment' => FieldRegistry::rootModel(TypeRegistry::payment(), Payment::class),
-                'payments' => FieldRegistry::rootList(TypeRegistry::payment(), Payment::class),
-                'product' => FieldRegistry::rootModel(TypeRegistry::product(), Product::class),
-                'products' => FieldRegistry::rootList(TypeRegistry::product(), Product::class),
-                'productLine' => FieldRegistry::rootModel(TypeRegistry::productLine(), ProductLine::class),
-                'productLines' => FieldRegistry::rootList(TypeRegistry::productLine(), ProductLine::class),
-            ],
+            'fields' => function () {
+                return [
+                    'customer' => FieldRegistry::rootModel(TypeRegistry::customer(), Customer::class),
+                    'customers' => FieldRegistry::rootList(TypeRegistry::customer(), Customer::class),
+                    'employee' => FieldRegistry::rootModel(TypeRegistry::employee(), Employee::class),
+                    'employees' => FieldRegistry::rootList(TypeRegistry::employee(), Employee::class),
+                    'office' => FieldRegistry::rootModel(TypeRegistry::office(), Office::class),
+                    'offices' => FieldRegistry::rootList(TypeRegistry::office(), Office::class),
+                    'order' => FieldRegistry::rootModel(TypeRegistry::order(), Order::class),
+                    'orders' => FieldRegistry::rootList(TypeRegistry::order(), Order::class),
+                    'orderDetail' => FieldRegistry::rootModel(TypeRegistry::orderDetail(), OrderDetail::class),
+                    'orderDetails' => FieldRegistry::rootList(TypeRegistry::orderDetail(), OrderDetail::class),
+                    'payment' => FieldRegistry::rootModel(TypeRegistry::payment(), Payment::class),
+                    'payments' => FieldRegistry::rootList(TypeRegistry::payment(), Payment::class),
+                    'product' => FieldRegistry::rootModel(TypeRegistry::product(), Product::class),
+                    'products' => FieldRegistry::rootList(TypeRegistry::product(), Product::class),
+                    'productLine' => FieldRegistry::rootModel(TypeRegistry::productLine(), ProductLine::class),
+                    'productLines' => FieldRegistry::rootList(TypeRegistry::productLine(), ProductLine::class),
+                ];
+            }
         ]);
     }
 }
